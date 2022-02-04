e8dc38df4cdb03f7e8a4bd3c222e1e2113b174b8
gabolezama <lezama_freddy@hotmail.com>
2022-02-04 01:00:13 -0300

    Crear app con Expo CLI

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{width:'90%', backgroundColor:'white'}}>
+        <TextInput style={{...StyleSheet.textIn, bordeBottomColor:'black'}}/>
+        <Button onPress={()=>{ alert('Epale!!')}} title="Add"/>
+      </View>
+      <Text>Hola, Coder!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +19,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  textIn:{
+     
+  },
+  myButton:{
+    marginTop:'10px'
+  }
 });
